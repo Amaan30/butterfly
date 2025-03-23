c803fb1586ae7acd799b8e5000f93b38a462c01c
Amaan <asifamaankhan20000412@gmail.com>
2025-03-24 05:00:41 +0530

    basic update

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/butterfly/',
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  },
   plugins: [
     tailwindcss(),
     react(),],
