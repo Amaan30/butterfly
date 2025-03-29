11df9f0a1f0c0a16fd2783be96f89799a035e599
Amaan <asifamaankhan20000412@gmail.com>
2025-03-29 12:05:24 +0530

    basic update

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -59,7 +59,7 @@ const Profile: React.FC = () => {
         
       </div>
       <div id="profileContainer" className='flex justify-center bg-blue-100 h-auto w-screen'>
-        <div id="profileCard" className='flex flex-col items-center justify-center bg-white p-4 my-4 w-full rounded-lg shadow-lg'>
+        <div id="profileCard" className='flex flex-col items-center justify-center bg-white p-4 my-4 w-full rounded-lg shadow-lg top-16'>
           <div id="profilePic" className='w-32 h-32 bg-contain rounded-full overflow-hidden mb-5'>
             <img src={user?.profilePicture} alt="Profile Picture" className="w-full h-full object-cover" />
           </div>
@@ -74,7 +74,7 @@ const Profile: React.FC = () => {
             <input type="text" value={count} min={0} max={200} onChange={(e) => setCount(Number(e.target.value))} />
             <p>{count}</p>
             <p id='line' className='w-full h-1 bg-black my-2'></p>
-            <div id="postsContainer" className='border-2 bg-amber-100 p-5 w-2/3 grid grid-flow-row grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+            <div id="postsContainer" className='border-2 bg-amber-100 p-5 w-2/3 grid grid-flow-row grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
               {Array.from({ length: count }).map((_, index) => (
                 <div key={index} id={`postExample${index}`} className='bg-indigo-300 border-2 w-32 aspect-square'>
                   <div id="postImage" className='w-full h-full bg-contain rounded-lg overflow-hidden mb-5'>
